caf2a298ab50c49c6158d0d25a993461b75561d7
Jeff Borda <jeffborda@outlook.com>
2020-08-29 13:21:11 -0700

    initial commit

---
+++ src/main/java/com/jeffborda/parkwestmi/ParkwestmiApplication.java
@@ -0,0 +1,13 @@
+package com.jeffborda.parkwestmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkwestmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkwestmiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffborda/parkwestmi/ParkwestmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffborda.parkwestmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkwestmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

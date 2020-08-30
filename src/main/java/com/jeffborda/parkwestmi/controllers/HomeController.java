05aeb5141ebbdac8110cd7c86446544b72e41241
Jeff Borda <jeffborda@outlook.com>
2020-08-30 11:47:55 -0700

    img directory restructure, fragment work, favicon linked

--- src/main/java/com/jeffborda/parkwestmi/controllers/HomeController.java
+++ src/main/java/com/jeffborda/parkwestmi/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package com.jeffborda.parkwestmi.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String getHome() {
+    public String getHome(Model m) {
+        m.addAttribute("pageTitle", "Parkwest");
         return "home";
     }
+
 }
